30480b948d4e784f8bf2379bc9e1750caefea620
Khanh Le <masterbruculous@gmail.com>
2024-06-14 20:36:08 +0700

    Initial Commit

---
+++ TiktokClone/TiktokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TiktokCloneApp.swift
+//  TiktokClone
+//
+//  Created by MasterBi on 14/6/24.
+//
+
+import SwiftUI
+
+@main
+struct TiktokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
